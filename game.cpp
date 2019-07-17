a369a26a405012818814304dacd800e569abac08
ryancody <ryan.p.cody@gmail.com>
2019-07-16 20:17:42 -0400

    full screen color change working

--- game.cpp
+++ game.cpp
@@ -3,6 +3,7 @@
 #include "Systems/GameObject.h"
 #include "tutorials/pointers.h"
 #include "Screen.h"
+#include <math.h>
 #include <SDL2/SDL.h>
 
 using namespace std;
@@ -23,9 +24,14 @@ int main () {
     while(true) {
         // update particles
         // draw particles
+        int elapsed = SDL_GetTicks();
+        unsigned char red = (unsigned char)((1 + sin(elapsed * 0.0001)) * 128);
+        unsigned char green = (unsigned char)((1 + sin(elapsed * 0.0002)) * 128);
+        unsigned char blue = (unsigned char)((1 + sin(elapsed * 0.0003)) * 128);
+        
         for(int y = 0; y < Screen::SCREEN_HEIGHT; y++) {
             for(int x = 0; x < Screen::SCREEN_WIDTH; x++) {
-                screen.setPixel(x, y, 0, 255, 0);
+                screen.setPixel(x, y, red, green, blue);
             }
         }
 
