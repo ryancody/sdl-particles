eef292dc97bb3432488d742673c066ca5d0a9403
ryancody <ryan.p.cody@gmail.com>
2019-07-16 21:25:59 -0400

    draw pixels to screen

--- game.cpp
+++ game.cpp
@@ -7,6 +7,7 @@
 #include <SDL2/SDL.h>
 #include <stdlib.h>
 #include <time.h>
+#include "Swarm.h"
 
 using namespace std;
 using namespace cppGame;
@@ -26,10 +27,25 @@ int main () {
 
     SDL_Event event;
 
+    Swarm swarm;
+
     while(true) {
         // update particles
         // draw particles
+
+        const Particle * const pParticles = swarm.getParticles();
+        for ( int i = 0; i < Swarm::NPARTICLES; i++ ) {
+            Particle particle = pParticles[i];
+
+            int x = (particle.m_x + 1) * Screen::SCREEN_WIDTH / 2;
+            int y = (particle.m_y + 1) * Screen::SCREEN_HEIGHT / 2;
+
+            screen.setPixel(x, y, 255, 255, 255);
+        } 
+        
+        /*
         int elapsed = SDL_GetTicks();
+        
         unsigned char red = (unsigned char)((1 + sin(elapsed * 0.0001)) * 128);
         unsigned char green = (unsigned char)((1 + sin(elapsed * 0.0002)) * 128);
         unsigned char blue = (unsigned char)((1 + sin(elapsed * 0.0003)) * 128);
@@ -39,6 +55,7 @@ int main () {
                 screen.setPixel(x, y, red, green, blue);
             }
         }
+        */
 
         // draw the screen
         screen.update();
